9a18ad0e7e924bcfbcc87fc181064e5ba086eda2
Anton <avonpoblozki@posteo.de>
2023-07-21 21:27:20 +0200

    require jwt token for createCart

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { requireAuth } = require("../middlewares/requireAuth");
 const { createCart } = require("../controllers/cart");
-
+router.use(requireAuth);
 router.post("/", createCart);
 module.exports = router;
